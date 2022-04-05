1071445ab363b3d43c0942bb9e3893b908068eac
Annalena <49262965+Lenare326@users.noreply.github.com>
2022-04-05 22:04:19 +0200

    solved task_1_2

--- task_1/task_1.py
+++ task_1/task_1.py
@@ -12,7 +12,14 @@ def count_increase(measurements):
 	return count
 
 
+def sliding_window(measurements):
+	count = sum([sum([m1, m2, m3]) < sum([m2, m3, m4]) for m1, m2, m3, m4 in zip(measurements, measurements[1:], measurements[2:], measurements[3:])])
+	return count
+
+
 if __name__ == '__main__':
 	arr = read_file("measurements.txt")
 	res = count_increase(arr)
+	res_window = sliding_window(arr)
 	print(str(res) + " measurements are larger than the previous one.")
+	print(str(res_window) + " measurements are larger than the previous one in the sliding window approach.")
